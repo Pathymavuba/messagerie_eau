104e27b8d1f6f903b3edd9c3d79e2e589c8f2388
math <mavubapathy@example.com>
2024-08-15 11:57:08 +0200

    add socket seerver

--- socket.js
+++ socket.js
@@ -1,50 +1,44 @@
-const app = require('./app')
+const app = require("./app");
 const { Server } = require("socket.io");
-const {createServer} = require("http")
-const cors = require('cors')
+const { createServer } = require("http");
+const cors = require("cors");
 
+const server = createServer(app);
 
-const server = createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
-const io = new Server(server,{
-    cors :{
-        origin:"*"
+//  store all users online
+global.onlineUsers = {};
+
+io.on("connection", (socket) => {
+  console.log(`new connexion ${socket.id}`);
+
+  global.chaTsocket = socket;
+
+  //store a user among online users when connected
+  socket.on("add-user", (userId) => {
+    onlineUsers[userId] = socket.id;
+  });
+
+  //send a message to a specific user
+  socket.on("send-message", async (data) => {
+    const sendUserSocket = onlineUsers[data.to];
+
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("receive", {
+        sender: data.sender,
+        conversation: data.conversation,
+        text: data.text,
+      });
     }
-})
+  });
 
-//  store all users online 
-global.onlineUsers = {}
-
-
-io.on("connection",(socket)=>{
-console.log(`new connexion ${socket.id}`)
-
-   global.chaTsocket = socket
-
-   //store a user among online users when connected
-   socket.on("add-user",(userId)=>{
-    onlineUsers[userId] = socket.id
-   })
-
-   //send a message to a specific user
-   socket.on('send-message', async (data)=>{
-    const sendUserSocket = onlineUsers[data.to]
-
-    if (sendUserSocket){
-        socket.to(sendUserSocket).emit("receive",{
-            sender:data.sender,
-            conversation:data.conversation,
-            text:data.text
-        })
-    }
-
-   })
-
-   //send a message when create a groupe 
-   socket.on('first-time',(data)=>{
-    socket.broadcast.emit("groupe-created",data)
-   })
-
-})
-
-
+  //send a message when create a groupe
+  socket.on("first-time", (data) => {
+    socket.broadcast.emit("groupe-created", data);
+  });
+});
