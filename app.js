5c74354ab2e253390a716810dd0c58d11e166086
math <mavubapathy@example.com>
2024-08-15 11:56:32 +0200

    add socket seerver

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 require("dotenv").config();
-require("./config/db")
+require("./config/db");
 
 
-var usersRouter = require('./routes/users');
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-app.use(cors())
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
+//message for not found
+app.all("*",(request,response)=>{
+  response.sendStatus(404)
+})
 
 app.get("/", (req, res) => {
-    res.send("Bienvenu dans l'API EAU");
-  });
-app.use('/users', usersRouter);
+  res.send("Bienvenu dans l'API EAU");
+});
+app.use("/users", usersRouter);
+
+
+
 
 module.exports = app;
---
+++ socket.js
@@ -0,0 +1,50 @@
+const app = require('./app')
+const { Server } = require("socket.io");
+const {createServer} = require("http")
+const cors = require('cors')
+
+
+const server = createServer(app)
+
+const io = new Server(server,{
+    cors :{
+        origin:"*"
+    }
+})
+
+//  store all users online 
+global.onlineUsers = {}
+
+
+io.on("connection",(socket)=>{
+console.log(`new connexion ${socket.id}`)
+
+   global.chaTsocket = socket
+
+   //store a user among online users when connected
+   socket.on("add-user",(userId)=>{
+    onlineUsers[userId] = socket.id
+   })
+
+   //send a message to a specific user
+   socket.on('send-message', async (data)=>{
+    const sendUserSocket = onlineUsers[data.to]
+
+    if (sendUserSocket){
+        socket.to(sendUserSocket).emit("receive",{
+            sender:data.sender,
+            conversation:data.conversation,
+            text:data.text
+        })
+    }
+
+   })
+
+   //send a message when create a groupe 
+   socket.on('first-time',(data)=>{
+    socket.broadcast.emit("groupe-created",data)
+   })
+
+})
+
+
